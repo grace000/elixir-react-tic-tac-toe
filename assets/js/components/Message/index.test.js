3c13ad278960dab0b83c15a62bd23fade76f8e7b
Tiffani <tiffani.grace.elmore@gmail.com>
2019-04-10 17:13:39 -0500

    add message tests for displaying messages with props

--- assets/js/components/Message/index.test.js
+++ assets/js/components/Message/index.test.js
@@ -8,4 +8,16 @@ describe('Message', () => {
 
         expect(wrapper.exists()).toBe(true);
     });
+
+    it(' renders player message', () => {
+        const wrapper = shallow(<Message message={"Player One"} />);
+
+        expect(wrapper.text()).toBe("Player One please select a spot on the board!");
+    })
+
+    it(' renders error message', () => {
+        const wrapper = shallow(<Message error={"Oh no, select another spot"} />);
+
+        expect(wrapper.text()).toBe(" please select a spot on the board!Oh no, select another spot");
+    })
 });
