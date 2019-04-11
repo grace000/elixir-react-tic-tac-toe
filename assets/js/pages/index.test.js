8c34b3fa0f8d82588dc190470d90e3b834f19093
Tiffani <tiffani.grace.elmore@gmail.com>
2019-04-11 09:37:28 -0500

    add game component tests

--- assets/js/pages/index.test.js
+++ assets/js/pages/index.test.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom'
 import { shallow, mount } from 'enzyme'
 import Game from './index'
 import Main from '../components/Main'
+import Message from '../components/Message'
+import Board from '../components/Board'
 
 describe('Game Component', () => {
     it(' renders welcome text', () => {
@@ -12,5 +14,23 @@ describe('Game Component', () => {
         const heroText = container.querySelector('.phx-hero');
 
         expect(heroText.textContent).toBe("Welcome to Tic Tac Toe!");
+    })
+
+    it(' renders main component', () => {
+        const wrapper = shallow(<Game />);
+
+        expect(wrapper.containsMatchingElement(Main)).toEqual(true)
+    })
+
+    it(' renders message component', () => {
+        const wrapper = shallow(<Game />);
+
+        expect(wrapper.containsMatchingElement(Message)).toEqual(true)
+    })
+    
+    it(' renders board component', () => {
+        const wrapper = shallow(<Game />);
+
+        expect(wrapper.containsMatchingElement(Board)).toEqual(true)
     })
 });
