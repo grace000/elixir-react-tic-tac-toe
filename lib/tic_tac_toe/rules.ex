f0e931956584e609353fc02fda5eabb05207c0d0
Tiffani <tiffani.grace.elmore@gmail.com>
2019-04-19 10:02:55 -0500

    place refactored rules on separate branch

--- lib/tic_tac_toe/rules.ex
+++ lib/tic_tac_toe/rules.ex
@@ -64,13 +64,22 @@ defmodule TicTacToe.Rules do
         |> Enum.all?(fn _item -> Kernel.match?(_item, @players) && _item != :empty end)
     end
 
+    # defp all_elements_in_list_equal?(lists) do
+    #     Enum.any?(lists, fn list -> 
+    #         if !Enum.member?(list, :empty) do
+    #             Enum.filter(list, &match?([x,x,x] when x in @players, &1))
+    #             Enum.all?
+    #         else
+    #             false
+    #         end
+    #     end)
+    # end
+
     defp all_elements_in_list_equal?(lists) do
         Enum.any?(lists, fn list -> 
-            if !Enum.member?(list, :empty) do
-                Enum.filter(list, &match?([x,x,x] when x in @players, &1))
-            else
-                false
-            end
+            Enum.map(list, fn item -> item end)
+            |> Enum.all?(fn _item -> Kernel.match?(_item, @players) && _item != :empty end)
+            
         end)
     end
 end
